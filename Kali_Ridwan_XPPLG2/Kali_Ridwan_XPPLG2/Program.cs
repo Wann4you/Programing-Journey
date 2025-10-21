fb96363f87ee1e2334a3eeb784018151a925e6d4
Wann4you <ridwan258762@student.smkn1kandeman.sch.id>
2025-10-21 17:56:32 +0700

    Add files via upload

---
+++ Kali_Ridwan_XPPLG2/Kali_Ridwan_XPPLG2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kali_Ridwan_XPPLG2
+{
+    internal class Program
+    {
+        static double HitungKali(double angka1, double angka2, double angka3)
+        {
+            double hasil = angka1 * angka2 * angka3;
+            return hasil;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan Angka Pertama   : ");
+            double angka1 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.Write("Masukkan Angka Kedua     : ");
+            double angka2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan Angka Ketiga    : ");
+            double angka3 = Convert.ToDouble(Console.ReadLine());
+
+
+            double hasil = HitungKali(angka1, angka2, angka3);
+            Console.WriteLine("Hasil perkalian = " + hasil);
+        }
+    }
+}
