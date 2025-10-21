f13bc920b82dbfc7752e20b2986308af930d2316
Wann4you <ridwan258762@student.smkn1kandeman.sch.id>
2025-10-21 17:37:54 +0700

    Add files via upload

---
+++ Praktik7.1_Ridwan_XPPLG2/Praktik7.1_Ridwan_XPPLG2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik7._1_Ridwan_XPPLG2
+{
+    internal class Program
+    {
+        static void sapa()
+        {
+            Console.WriteLine("Halo selamat datang di pembelajaran C#!");
+            Console.WriteLine("Mari belajar tentang fungsi");
+        }
+        static void Main(string[] args)
+        {
+            sapa(); // Memanggil fungsi
+        }
+    }
+}
---
+++ Praktik7.2_Ridwan_XPPLG2/Praktik7.2_Ridwan_XPPLG2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik7._2_Ridwan_XPPLG2
+{
+    internal class Program
+    {
+        static void sapaNama(string nama)
+        {
+            Console.WriteLine("Halo, " + nama + "!");
+            Console.WriteLine("Semangat belajar C# hari ini! ");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write(" Masukkan Nama Anda : ");
+            string namaSiswa = Console.ReadLine();
+            sapaNama(namaSiswa); // Memanggil fungsi dengan argumen
+        }
+    }
+}
